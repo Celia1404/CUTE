c88141eb84a20f6906b9bb949f2c325a9034f8e1
Celia1404 <112662792+Celia1404@users.noreply.github.com>
2024-02-04 19:34:13 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+    question.innerHTML = "Yes, I love you too!";
+    gif.src = "peach-goma-love.gif";
+});
+
+noBtn.addEventListener('mouseover', () => {
+    const noBtnRec = noBtn.getBoundingClientRect();
+    const maxX = window.innerWidth - noBtnRec.width;
+    const maxY = window.innerHeight - noBtnRec.height;
+
+    const randomX = Math.floor(Math.random() * maxX);
+    const randomY = Math.floor(Math.random() * maxY);
+
+    noBtn.style.position = 'absolute';
+    noBtn.style.left = randomX + 'px';
+    noBtn.style.top = randomY + 'px';
+});
